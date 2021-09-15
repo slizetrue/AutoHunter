507c71dbf5fee016f0fcca219bf757db43514055
SLIZE <71553661+slizetrue@users.noreply.github.com>
2021-09-15 09:11:15 +0400

    jaggu.py

--- cogs/josh.py
+++ cogs/josh.py
@@ -4,14 +4,14 @@ import random
 from discord.ext import commands
 from time import sleep
 
-class Fun(commands.Cog):
+class Socials(commands.Cog):
 
     def _init_(self, client):
         self.client = client
 
     @commands.command()
-    async def josh(self, ctx):
-        await ctx.send(f'https://www.youtube.com/channel/UCYI6ZNNlu1vdaY7yzo1Z66w')
+    async def aplhayt(self, ctx):
+        await ctx.send(f'https://www.youtube.com/channel/UCpliXPL-kvCX6PTuE-2dAvA')
 
 
 def setup(client):
